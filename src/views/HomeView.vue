420fab5eacbd1e8879fefdae17d034a940b2e61c
hilmy uzz <hbuzzaman@gmail.com>
2024-04-03 07:25:51 +0700

    first cmt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderShayna />
+    <hero-shayna />
+    <WomenShayna />
+    <InstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import WomenShayna from '@/components/WomenShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerShayna from '@/components/PartnerShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+// import carousel from 'vue-owl-carousel' //instal dependencies carousel dengan vue UI
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HeaderShayna,
+    // carousel, //pindah ke HeroShayna.vue 
+    HeroShayna,
+    WomenShayna,
+    InstaShayna,
+    PartnerShayna,
+    FooterShayna,
   }
 }
 </script>
